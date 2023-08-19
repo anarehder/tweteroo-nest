ac9ba65bd911d9298e9d921e175b4fc0781b9ce7
anarehder <anapaula.rehder@gmail.com>
2023-08-19 11:17:52 -0300

    feat/ post usuarios

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateUserDto } from './dtos/user.dto';
+import { User } from './entities/user.entity';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @HttpCode(200)
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('sign-up')
+  @HttpCode(200)
+  createUser(@Body() body: CreateUserDto): string {
+    try {
+      return this.appService.createUser(body);
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(error.message, HttpStatus.UNAUTHORIZED);
+    }
+  }
+
+  @Get('user')
+  @HttpCode(200)
+  getUsers(): User[] {
+    return this.appService.getUsers();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { User } from './entities/user.entity';
+//import { CreateUserDto } from './dtos/user.dto';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return "I'm okay!";
+  }
+
+  private users: User[]; // persistencia em memoria
+
+  constructor() {
+    this.users = [];
+  }
+
+  createUser(body): string {
+    if (this.users.find((user) => user.username === body.username)) {
+      throw new UnauthorizedException('This username already exists');
+    } else {
+      this.users.push(body);
+    }
+
+    return 'OK';
+  }
+
+  getUsers() {
+    console.log(this.users);
+    return this.users;
   }
 }
